84d6091fff3e1fd23aa8fe1a8d80dbbdad83b59e
webflixinfo <kontakt@webflix.info>
2026-01-10 01:42:45 +0100

    Updated HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -82,7 +82,7 @@ export default function HeroSection({ onOpenForm }: HeroSectionProps) {
                 <img
                   src="https://i.imgur.com/AROdPLs.jpg"
                   alt="Professioneller Sicherheitsdienst"
-                  className="w-full h-[300px] object-cover object-[center_20%] grayscale contrast-110 group-hover/image:grayscale-0 group-hover/image:scale-105 transition-all duration-700 ease-out"
+                  className="w-full h-[300px] object-cover object-[center_20%] contrast-110 group-hover/image:scale-105 transition-all duration-700 ease-out"
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-black via-black/30 to-transparent"></div>
                 <div
@@ -146,7 +146,7 @@ export default function HeroSection({ onOpenForm }: HeroSectionProps) {
                 <img
                   src="https://i.imgur.com/AROdPLs.jpg"
                   alt="Professioneller Sicherheitsdienst"
-                  className="w-full h-[600px] object-cover object-[center_20%] grayscale contrast-110 group-hover/image:grayscale-0 group-hover/image:scale-105 transition-all duration-700 ease-out"
+                  className="w-full h-[600px] object-cover object-[center_20%] contrast-110 group-hover/image:scale-105 transition-all duration-700 ease-out"
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-black via-black/30 to-transparent"></div>
                 <div
